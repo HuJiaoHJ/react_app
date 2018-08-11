fbe19a861a75ebcf81cf5e96d1360671bf913aac
hujiao <hujiao@bytedance.com>
2018-08-11 22:55:48 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from './react-dom/react-dom.development';
 import './index.css';
 import App from './App';
 
+console.log(<App />)
+
 ReactDOM.render(<App />, document.getElementById('root'));
